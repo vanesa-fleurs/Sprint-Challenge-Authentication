169762fb81e02082a15c810b7ec7c6ce2d57c10b
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-18 11:25:48 -0500

    fixed typo in dbConfig

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -4,4 +4,4 @@ const knexConfig = require('../knexfile.js');
 
 const environment = process.env.DB_ENV || 'development';
 
-module.exports = knex(config[environment])
+module.exports = knex(knexConfig[environment])
