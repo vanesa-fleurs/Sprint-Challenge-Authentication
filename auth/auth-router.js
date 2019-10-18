3399df9f2e44919ef6b235726d786a1127effdc0
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-18 11:04:04 -0500

    done setting up

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
-const restricted = require('./authenticate-middleware.js');
+const restricted = require('./authenticate-middleware.js'); //?
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const Users = require('../helpers/users-model.js')
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+      .then(saveU => {
+          console.log('Save /register: ', saveU)
+          res.status(201).json(saveU)
+      })
+      .catch(error => {
+        console.log(error)
+          res.status(500).json({ message: 'cannot add the user' });
+      });
 });
 
 router.post('/login', (req, res) => {
