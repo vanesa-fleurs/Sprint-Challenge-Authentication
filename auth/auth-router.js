f5f51ac90f99f88ea25f5b98e85c6fbf60056ef7
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-18 11:41:17 -0500

    login with token ready

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -22,7 +22,36 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+      .then(user => {
+          if(user && bcrypt.compareSync(password, user.password)){
+              const token = generateToken(user);
+              res.status(200).json({
+                  message: `Welcome ${user.username}!`,
+                  token,
+                });
+          }
+          else{
+              res.status(401).json({ message: 'Invalid Credentials' })
+          }
+      })
+      .catch(error => {
+          res.status(500).json(error);
+      })
 });
+
+
+function generateToken(user){
+  const payload = {
+      username: user.username,
+      subject:user.id
+  };
+  const options ={
+      expiresIn: '1d'
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
 
 module.exports = router;
