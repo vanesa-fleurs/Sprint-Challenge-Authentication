dac859e6d5884ee2f009e04bd40d54e241a07e0c
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-15 10:43:52 -0600

    models

---
+++ models/users-model.js
@@ -0,0 +1,28 @@
+const db = require('../knexfile.js')
+module.exports = {
+    getAll,
+    add,
+    findById,
+    findBy
+}
+
+function getAll(){
+    return db('users')
+        .select('id', 'username')
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user);
+    return findById(id);
+}
+
+function findById(id) {
+    return db('users')
+      .select('id', 'username')
+      .where({ id })
+      .first();
+}
+
+function findBy(filter){
+    return db('users').where(filter).first();
+}
